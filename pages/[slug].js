11ca4842dcb7c586886d5fa38fcaeba0a4c0f5f1
Godwill Barasa <iamgodwillb@gmail.com>
2021-04-18 02:23:25 +0300

    slug.js

---
+++ pages/[slug].js
@@ -0,0 +1,30 @@
+export default function Posts(){
+return(
+<div>{post.Title}</div>
+)
+}
+
+//tell next.js how many pages
+export async function getStaticPaths(){
+    const res = await fetch('http://localhost:1337/posts');
+    const posts = await res.json();
+
+    const paths = posts.map((post =>({
+       params: {slug: post.Slug},
+    }));
+
+    return{
+        paths:[],
+        fallback:false
+    }
+};
+
+// for each individual page
+export async function getStaticProps({ params }){
+
+    const { slug } = params;
+
+    const res = await fetch('http://localhost:1337/posts?slug=${slug}');
+    const data = await res.json();
+    const post =data[0];
+};
